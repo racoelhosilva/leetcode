406eee5196e2b785fd0bcc76afbc25f0994f43b1
Rodrigo Coelho <racoelhosilva@gmail.com>
2025-02-19 17:19:47 +0000

    Leetcode problem 110: Balanced Binary Tree

---
+++ Binary Trees/Easy/0110-BalancedBinaryTree.py
@@ -0,0 +1,53 @@
+"""
+LeetCode Problem: Balanced Binary Tree
+Problem Number: 110
+Difficulty: Easy
+Topic: Binary Trees
+Link: https://leetcode.com/problems/balanced-binary-tree/
+"""
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    # Top-Down Approach
+    # For each node, check if it is balanced by comparing heights of both subtrees
+    # Recursively check if balanced for the left and right subtrees
+    # Time Complexity: O(n^2)
+    # Space Complexity: O(n)
+    def isBalanced(self, root):
+        if not root:
+            return True
+        return abs(self.maxHeight(root.left) - self.maxHeight(root.right)) <= 1 and \
+            self.isBalanced(root.left) and self.isBalanced(root.right)
+    def maxHeight(self, root):
+        if not root: 
+            return 0
+        return 1 + max(self.maxHeight(root.left), self.maxHeight(root.right))
+    
+    # Bottom-Up Approach
+    # To avoid recalculating heights and checking balances of nodes,
+    # We can use a modified height function that returns -1 if any of the 
+    # subtrees is not balanced and propagate this value up the tree
+    # Time Complexity: O(n)
+    # Space Complexity: O(1)
+    def isBalanced(self, root):
+        return self.balancedHeight(root) >= 0
+    def balancedHeight(self, root):
+        if not root:
+            return 0
+        
+        left = self.balancedHeight(root.left)
+        if left < 0: 
+            return left
+        right = self.balancedHeight(root.right)
+        if right < 0: 
+            return right
+
+        if abs(left - right) > 1: 
+            return -1
+        return 1 + max(left, right)
--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 | Arrays & Hashing | 2 | 0 | 0 | 2 |  |
 | Backtracking | 0 | 0 | 0 | 0 |
 | Binary Search | 1 | 0 | 0 | 1 |
-| Binary Trees | 1 | 1 | 0 | 2 |
+| Binary Trees | 2 | 1 | 0 | 3 |
 | Bit Manipulation | 0 | 0 | 0 | 0 |
 | Dynamic Programming 1D | 0 | 0 | 0 | 0 |
 | Dynamic Programming 2D | 0 | 0 | 0 | 0 |
@@ -20,4 +20,4 @@
 | Stack | 1 | 0 | 0 | 1 |
 | Tries | 0 | 0 | 0 | 0 |
 | Two Pointers | 1 | 0 | 0 | 1 |
-| **Totals** | 9 | 1 | 0 | 10 |
+| **Totals** | 10 | 1 | 0 | 11 |
